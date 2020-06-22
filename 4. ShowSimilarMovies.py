d53f6f86290f14e6fa458ac94eb954fd0701ae4d
Amogh R <amoghpavan5363@gmail.com>
2020-06-22 18:05:22 +0530

    Create 4. ShowSimilarMovies.py

---
+++ 4. ShowSimilarMovies.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+r_cols = ['user_id', 'movie_id', 'rating']
+ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols, usecols=range(3), encoding="ISO-8859-1")
+
+m_cols = ['movie_id', 'title']
+movies = pd.read_csv('ml-100k/u.item', sep='|', names=m_cols, usecols=range(2), encoding="ISO-8859-1")
+
+ratings = pd.merge(movies, ratings)
+
+movieRatings = ratings.pivot_table(index=['user_id'],columns=['title'],values='rating')
+
+starWarsRatings = movieRatings['Star Wars (1977)']
+
+similarMovies = movieRatings.corrwith(starWarsRatings)
+similarMovies = similarMovies.dropna()
+df = pd.DataFrame(similarMovies)
+
+movieStats = ratings.groupby('title').agg({'rating': [np.size, np.mean]})
+popularMovies = movieStats['rating']['size'] >= 100
+
+df = movieStats[popularMovies].join(pd.DataFrame(similarMovies, columns=['similarity']))
+df.sort_values(['similarity'], ascending=False)[:15]
